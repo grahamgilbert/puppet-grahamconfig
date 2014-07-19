1d4b95f10685904f9c7976032aefcbdf7caa6cbd
Graham Gilbert <graham@grahamgilbert.com>
2014-07-19 08:33:47 +0100

    Default file ownership

--- manifests/config.pp
+++ manifests/config.pp
@@ -5,6 +5,17 @@ class grahamconfig::config (
     $my_sourcedir = $grahamconfig::my_sourcedir
     $my_username  = $grahamconfig::my_username
     
+    File {
+        owner => $my_username,
+        group => 'staff'
+    }
+
+    if !defined(File["${my_sourcedir}"]){
+        file {"${my_sourcedir}":
+            ensure => directory,
+        }
+    }
+
     if !defined(File["${my_sourcedir}/Mine"]){
         file {"${my_sourcedir}/Mine":
             ensure => directory,
---
+++ manifests/params.pp
@@ -0,0 +1,6 @@
+class grahamconfig::params {
+  # $::luser and $::boxen_srcdir come from Boxen's custom facts
+  $my_username  = 'grahamgilbert'
+  $my_homedir   = "/Users/grahamgilbert"
+  $my_sourcedir = "/Users/grahamgilbert/src"
+}

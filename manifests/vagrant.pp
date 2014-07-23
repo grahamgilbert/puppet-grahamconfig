2d1ca4ecf3500d1f0bf545137b90f31d25be2d36
Graham Gilbert <graham@grahamgilbert.com>
2014-07-23 14:41:34 +0100

    Vagrant licensing

--- manifests/vagrant.pp
+++ manifests/vagrant.pp
@@ -5,6 +5,25 @@ class grahamconfig::vagrant (
     $my_username  = $grahamconfig::my_username
 
     vagrant::plugin { 'vagrant-vmware-fusion':
-        license => "${my_homedir}/Dropbox/Config/Vagrant/fusion-6-license.lic",
+        $license = "${my_homedir}/Dropbox/Config/Vagrant/fusion-6-license.lic",
+    }
+
+    File {
+        owner => $my_username,
+        group => 'staff'
+    }
+
+    file { "/Users/${my_username}/.vagrant.d":
+        ensure => directory,
+    }
+
+    file { "/Users/${my_username}/.vagrant.d/license-vagrant-vmware-fusion.lic":
+      ensure  => present,
+      mode    => '0644',
+      source  => $license,
+    }
+
+    vagrant_plugin { 'vagrant-vmware-fusion':
+        ensure => present
     }
 }

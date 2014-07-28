443e3070fb0c5b4705a74ac184d327b54ae97d65
Graham Gilbert <graham@grahamgilbert.com>
2014-07-28 10:42:57 +0100

    I'll come back to this

--- manifests/config.pp
+++ manifests/config.pp
@@ -131,13 +131,13 @@ class grahamconfig::config (
         user    => $my_username,
     }
 
-    mac_admin::osx_defaults { 'Remove Alfred Hat from the Menu Bar':
-        domain  => "/Users/${my_username}/Library/Application Support/Alfred 2/Alfred.alfredpreferences/preferences/appearance/options/prefs.plist",
-        key     => 'hidemenu',
-        value   => 'true',
-        type    => bool,
-        user    => $my_username,
-    }
+    # mac_admin::osx_defaults { 'Remove Alfred Hat from the Menu Bar':
+    #     domain  => "/Users/${my_username}/Library/Application Support/Alfred 2/Alfred.alfredpreferences/preferences/appearance/options/prefs.plist",
+    #     key     => 'hidemenu',
+    #     value   => 'true',
+    #     type    => bool,
+    #     user    => $my_username,
+    # }
 
     mac_admin::osx_defaults { 'Set Alfred theme to Lion':
         domain  => 'com.runningwithcrayons.Alfred-Preferences',

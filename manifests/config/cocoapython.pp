2ea1adf9d943d539c979003f653ff3ebc75c9263
Graham Gilbert <graham@grahamgilbert.com>
2015-04-03 13:16:31 +0100

    Xcode 6 templates! WOOO

--- manifests/config/cocoapython.pp
+++ manifests/config/cocoapython.pp
@@ -6,9 +6,9 @@ class grahamconfig::config::cocoapython (
     $my_username  = $grahamconfig::my_username
 
     # CocoaPython Template for Xcode
-    repository { 'Xcode5CocoaPythonTemplates':
-        source => 'gregneagle/Xcode5CocoaPythonTemplates',
-        path   => "${my_sourcedir}/Others/Xcode5CocoaPythonTemplates",
+    repository { 'Xcode6CocoaPythonTemplates':
+        source => 'gregneagle/Xcode6CocoaPythonTemplates',
+        path   => "${my_sourcedir}/Others/Xcode6CocoaPythonTemplates",
     }
 
     file { "/Users/${my_username}/Library/Developer":
@@ -21,8 +21,8 @@ class grahamconfig::config::cocoapython (
 
     file { "/Users/${my_username}/Library/Developer/Xcode/Templates":
         ensure  => link,
-        target  => "${my_sourcedir}/Others/Xcode5CocoaPythonTemplates",
-        require => Repository['Xcode5CocoaPythonTemplates']
+        target  => "${my_sourcedir}/Others/Xcode6CocoaPythonTemplates",
+        require => Repository['Xcode6CocoaPythonTemplates']
     }
 
 }

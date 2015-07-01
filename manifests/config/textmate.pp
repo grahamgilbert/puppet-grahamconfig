7508dc3febd25c43c1f5351d88023c15cf5adaea
Graham Gilbert <graham@grahamgilbert.com>
2015-07-01 13:21:07 +0200

    I don't care about this anymore

--- manifests/config/textmate.pp
+++ manifests/config/textmate.pp
@@ -12,12 +12,12 @@ class grahamconfig::config::textmate (
         path    => "${my_sourcedir}/Others/puppet-textmate-bundle",
     }
 
-    file { "/Users/${my_username}/Library/Application Support/TextMate/Managed/Bundles/Puppet.tmbundle":
-        ensure  => link,
-        force   => true,
-        target  => "${my_sourcedir}/Others/puppet-textmate-bundle",
-        require => Repository['puppet-textmate-bundle']
-    }
+    # file { "/Users/${my_username}/Library/Application Support/TextMate/Managed/Bundles/Puppet.tmbundle":
+    #     ensure  => link,
+    #     force   => true,
+    #     target  => "${my_sourcedir}/Others/puppet-textmate-bundle",
+    #     require => Repository['puppet-textmate-bundle']
+    # }
 
     mac_admin::osx_defaults { 'TextMate File Browser Placement':
         ensure => present,

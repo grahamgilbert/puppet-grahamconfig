d54ba1b976a32398b6c5c8b8b92091a7c9fc6ffb
Graham Gilbert <graham@grahamgilbert.com>
2015-07-24 16:16:30 +0100

    Remove chrome. I keep opening it

--- files/dock/dock.sh
+++ files/dock/dock.sh
@@ -4,7 +4,6 @@
 /usr/local/bin/dockutil --add '/Applications/Google Chrome.app' --no-restart
 /usr/local/bin/dockutil --add '/Applications/Safari.app' --after 'Google Chrome' --no-restart
 /usr/local/bin/dockutil --add '/Applications/Sublime Text.app' --no-restart
-/usr/local/bin/dockutil --add '/Applications/Atom.app' --no-restart
 /usr/local/bin/dockutil --add '/Applications/iTerm.app' --no-restart
 /usr/local/bin/dockutil --add '/Applications/Tweetbot.app' --no-restart
 /usr/local/bin/dockutil --add '/Applications/Trello.app' --no-restart

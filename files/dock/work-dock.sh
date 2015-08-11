8d38825e40ff2ac4dd68a8430a7451d637ffb718
Graham Gilbert <graham@grahamgilbert.com>
2015-08-11 12:32:31 +0100

    New outlook

--- files/dock/work-dock.sh
+++ files/dock/work-dock.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-/usr/local/bin/dockutil --add '/Applications/Microsoft Office 2011/Microsoft Outlook.app' --after 'Safari'
+/usr/local/bin/dockutil --add '/Applications/Microsoft Outlook.app' --after 'Safari'
 /usr/local/bin/dockutil --add '/Applications/Microsoft Lync.app' --after 'Trello'

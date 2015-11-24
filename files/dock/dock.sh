dc5ccae35bbc894f68e0953526715df8c05cbdd0
Graham Gilbert <graham@grahamgilbert.com>
2015-11-24 21:35:27 +0000

    Atom in the Dock

--- files/dock/dock.sh
+++ files/dock/dock.sh
@@ -3,7 +3,8 @@
 /usr/local/bin/dockutil --remove all --no-restart
 /usr/local/bin/dockutil --add '/Applications/Google Chrome.app' --no-restart
 /usr/local/bin/dockutil --add '/Applications/Safari.app' --after 'Google Chrome' --no-restart
-/usr/local/bin/dockutil --add '/Applications/Sublime Text.app' --no-restart
+#/usr/local/bin/dockutil --add '/Applications/Sublime Text.app' --no-restart
+/usr/local/bin/dockutil --add '/Applications/Atom.app' --no-restart
 /usr/local/bin/dockutil --add '/Applications/iTerm.app' --no-restart
 /usr/local/bin/dockutil --add '/Applications/Tweetbot.app' --no-restart
 /usr/local/bin/dockutil --add '/Applications/Clear.app' --no-restart

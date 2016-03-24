50d4e41e5bdc0fe592f04a5af30981241d2cb044
Graham Gilbert <graham@grahamgilbert.com>
2016-03-24 15:40:05 +0000

    Remove order on homebrew

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,18 +3,18 @@ class grahamconfig (
     $my_homedir   = '/Users/grahamgilbert',
     $my_sourcedir = '/Users/grahamgilbert/src',
     ){
-    class {'grahamconfig::gems': } ->
-    class {'grahamconfig::directories': } ->
-    class {'grahamconfig::repositories': } ->
-    class {'grahamconfig::config': } ->
-    class {'grahamconfig::dock': } ->
-    class {'grahamconfig::wallpaper': } ->
-    class {'grahamconfig::fonts': } ->
-    class {'grahamconfig::ssh_keys': } ->
-    class {'grahamconfig::git': } ->
-    class {'grahamconfig::homebrew': } ->
-    class {'grahamconfig::osquery': } ->
-    class {'grahamconfig::crashplan': }
-    class {'grahamconfig::munki': }
+        class {'grahamconfig::homebrew': }
+        class {'grahamconfig::gems': } ->
+        class {'grahamconfig::directories': } ->
+        class {'grahamconfig::repositories': } ->
+        class {'grahamconfig::config': } ->
+        class {'grahamconfig::dock': } ->
+        class {'grahamconfig::wallpaper': } ->
+        class {'grahamconfig::fonts': } ->
+        class {'grahamconfig::ssh_keys': } ->
+        class {'grahamconfig::git': } ->
+        class {'grahamconfig::osquery': } ->
+        class {'grahamconfig::crashplan': }
+        class {'grahamconfig::munki': }
 
 }

a839f16f0f0a9f7f2c1c423eea69826d3e57c946
Graham Gilbert <graham@grahamgilbert.com>
2016-03-31 09:52:51 +0100

    Didn't seem to work well

--- manifests/homebrew.pp
+++ manifests/homebrew.pp
@@ -4,22 +4,7 @@ class grahamconfig::homebrew {
         group => 'staff',
     } ->
 
-    exec { '/usr/local/bin/brew tap argon/mas':
-        user => 'grahamgilbert',
-        creates => '/usr/local/homebrew/Library/Taps/argon/homebrew-mas'
-    } ->
-
     package {'youtube-dl':
-        ensure   => 'present',
-        provider => 'brew',
-    } ->
-
-    # package {'argon/mas':
-    #     ensure   => 'present',
-    #     provider => 'tap',
-    # } ->
-
-    package {'mas':
         ensure   => 'present',
         provider => 'brew',
     } ->

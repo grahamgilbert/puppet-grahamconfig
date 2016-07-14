c5e1eb733033c9faedd5206ff4caae940bf420e3
Graham Gilbert <graham@grahamgilbert.com>
2016-07-14 12:21:38 +0100

    remove github

--- files/dock/dock.sh
+++ files/dock/dock.sh
@@ -9,5 +9,4 @@
 /usr/local/bin/dockutil --add '/Applications/Clear.app' --no-restart
 /usr/local/bin/dockutil --add '/Applications/Slack.app' --no-restart
 /usr/local/bin/dockutil --add '/Applications/VMware Fusion.app' --no-restart
-/usr/local/bin/dockutil --add '/Applications/GitHub Desktop.app' --no-restart
 /usr/local/bin/dockutil --add '~/Downloads' --view auto --display stack --sort dateadded

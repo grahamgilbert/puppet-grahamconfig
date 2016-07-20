ac1592f2144cff3f7cb13276f69dd5a5c4b4f551
Graham Gilbert <graham@grahamgilbert.com>
2016-07-20 15:19:59 +0100

    Don't use clear anymore

--- files/dock/dock.sh
+++ files/dock/dock.sh
@@ -6,7 +6,6 @@
 /usr/local/bin/dockutil --add '/Applications/Atom.app' --no-restart
 /usr/local/bin/dockutil --add '/Applications/iTerm.app' --no-restart
 /usr/local/bin/dockutil --add '/Applications/Tweetbot.app' --no-restart
-/usr/local/bin/dockutil --add '/Applications/Clear.app' --no-restart
 /usr/local/bin/dockutil --add '/Applications/Slack.app' --no-restart
 /usr/local/bin/dockutil --add '/Applications/VMware Fusion.app' --no-restart
 /usr/local/bin/dockutil --add '~/Downloads' --view auto --display stack --sort dateadded
